9c91d633ab4e6601ac47b16cb4a8fc0030fe489a
Ashwin Solanki <aswinsolanki76@gmail.com>
2022-07-17 01:46:44 +0530

    Halfway through

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import Card from "./Sparkliner/card";
+import "./home.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Vercel😎
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Card />
     </div>
   );
 }
---
+++ src/Sparkliner/card.jsx
@@ -0,0 +1,12 @@
+import "./card.css";
+
+function Card() {
+    // const s={"width:100%"};
+    return (
+            <div className="card">
+                <h1>SPARKLINER</h1>
+            </div>
+    );
+}
+
+export default Card;
